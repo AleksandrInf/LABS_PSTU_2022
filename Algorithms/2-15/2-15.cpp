82202620908ae3ecb130105f3fd1f1e9687534c0
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2022-11-30 00:34:37 +0500

    Add files via upload

---
+++ Algorithms/2-15/2-15.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n, s, k;
+    bool flag = false;
+
+    cin >> n >> s;
+
+    while (n > 0 && !flag)
+    {
+        k = n % 10;
+        if (k == s)
+        {
+            flag = true;
+            cout << "Öטפנא םאיהוםא" << endl;
+        }
+        else
+        {
+            n /= 10;
+        }
+    }
+
+    if (!flag)
+    {
+        cout << "Öטפנא םו םאיהוםא" << endl;
+    }
+
+    return 0;
+}
