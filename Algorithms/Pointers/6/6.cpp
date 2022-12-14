63350ce8525061543bceaf2f6c0413b5af498466
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2022-12-14 13:59:57 +0500

    Add files via upload

---
+++ Algorithms/Pointers/6/6.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<string>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b;
+    a = b = 0;
+
+    std::string s;
+    std::cout << "Ââåäèòå ñòðîêó:" << std::endl;
+    std::cin >> s;
+
+    const char* sptr = s.c_str();
+    for (int i = 0; i < s.length(); i++)
+    {
+        switch (*sptr)
+        {
+        case 'À':
+        case 'Å':
+        case 'È':
+        case 'Î':
+        case 'Ó':
+        case 'Ý':
+        case 'Þ':
+        case 'Û':
+        case 'ß':
+        case 'à':
+        case 'å':
+        case 'è':
+        case 'î':
+        case 'ó':
+        case 'ý':
+        case 'þ':
+        case 'û':
+        case 'ÿ':
+            a++;
+            break;
+        default:
+            b++;
+            break;
+        }
+
+        *sptr++;
+
+    }
+
+    std::cout << "Êîëè÷åñòâî ãëàñíûõ = " << a << std::endl;
+    std::cout << "Êîëè÷åñòâî ñîãëàñíûõ = " << b << std::endl;
+
+    return 0;
+}
