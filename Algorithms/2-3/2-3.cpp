c375538a644e5cd98e8ca985370c2b8ec771df46
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2022-12-20 16:36:47 +0300

    Update 2-3.cpp

--- Algorithms/2-3/2-3.cpp
+++ Algorithms/2-3/2-3.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-	int a;
-	cin >> a;
-	int presum = 0;
-	int sum = 0;
-	int c;
-
-	for (int i = 1; i < a + 1; i++)
-	{
-		presum = i;
-		c = i;
-		for (int j = 0; j < i; j++)
-		{
-			c++;
-			presum *= c;
-		}
-		sum += presum;
-	}
-	cout << sum;
-}
+    setlocale(LC_ALL, "Rus");
+    int n;
+    cout << "Введите число N" << endl;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int k = 1;
+        for (int j = 0; j <= i; j++)
+        {
+            k *= i + j;
+        }
+        sum += k;
+    }
     
+    cout << sum << endl;
+
+    return 0;
+}
 
