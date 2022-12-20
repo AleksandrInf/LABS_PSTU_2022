f73e75dc2dd9314044013b5358e71ff8b2be3ae3
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2022-12-20 16:50:09 +0500

    Add files via upload

---
+++ Algorithms/2-18/2-18.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int N;
+	int r = 0;
+
+	cout << "Введите натуральное число N" << endl;
+	cin >> N;
+
+	while (N > 0)
+	{
+		r = r * 10 + N % 10;
+		N /= 10;
+	}
+	cout << "Введенное число в обратном порядке будет выглядеть так: " << r << endl;
+
+	return 0;
+}
