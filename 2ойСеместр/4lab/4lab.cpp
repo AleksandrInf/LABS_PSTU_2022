562b47926a7453728d2649dd86750a8de48ffa7e
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2023-02-19 14:22:09 +0300

    Add files via upload

---
+++ 2ойСеместр/4lab/4lab.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int matrix[10][10];
+void strmatrix(int i, int j);
+
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			matrix[i][j] = rand() % 101;
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++); {
+			if (matrix[])
+		}
+	}
+}
