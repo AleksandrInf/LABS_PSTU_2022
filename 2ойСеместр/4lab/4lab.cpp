98ccad1bdac30c612f6781a4fc7e2811136ea95b
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2023-02-19 14:29:02 +0300

    Update 4lab.cpp

--- 2ойСеместр/4lab/4lab.cpp
+++ 2ойСеместр/4lab/4lab.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int matrix[10][10];
-void strmatrix(int i, int j);
-
-
 int main() {
+	int a[10];
 	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			matrix[i][j] = rand() % 101;
+		a[i] = rand() % 100 - 50;
+		cout << a[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < 10; i++) {
+		if (a[i] % 7 != 0) {
+			cout << a[i] << ' ';
 		}
 	}
+	cout << endl;
+	
 	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++); {
-			if (matrix[])
+		if (a[i] % 2 != 0) {
+			cout << a[i] << ' ' << '0' << ' ';
 		}
 	}
+	return 0;
 }
