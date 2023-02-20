d9761d8634d80d38ca50cf0dbbf3db34d55ed2c7
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2023-02-20 09:52:02 +0300

    Add files via upload

---
+++ 2ойСеместр/5lab/5lab.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+double dotProduct(vector<double> v1, vector<double> v2) {
+	double result = 0.0;
+	for (int i = 0; i < v1.size(); i++) {
+		result += v1[i] * v2[i];
+
+	}
+	return result;
+}
+
+int main() {
+	vector<double> v1 = { 1.0, 2.0, 3.0 };
+	vector<double> v2 = { 4.0, 5.0, 6.0 };
+	double result = dotProduct(v1, v2);
+	cout << "—кал€рное произведение векторов v1 и v2 равно = " << result << endl;
+	return 0;
+}
